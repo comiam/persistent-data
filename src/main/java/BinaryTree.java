9e5506f97bd375a5fe63d33dccd4b2fa84130116
Alexander Shchelokov <edgyestlord@gmail.com>
2022-12-24 19:15:04 +0700

    init rb tree

---
+++ src/main/java/BinaryTree.java
@@ -0,0 +1,27 @@
+import java.util.Map;
+
+enum Color {
+    Red,
+    Black
+}
+
+
+public class  BinaryTree<TK, TV> implements Map.Entry<TK, TV> {
+    @Override
+    public TK getKey() {
+        return null;
+    }
+
+    @Override
+    public TV getValue() {
+        return null;
+    }
+
+    @Override
+    public TV setValue(TV value) {
+        return null;
+    }
+
+
+
+}
